68a1f731c4f84e2e8926a5bd17ebdeef55ce70f2
mikk_leini <mikk_leini@91aef0f8-0833-0410-9fe2-cf865ed08620>
2007-07-17 19:45:31 +0000

    First version. Based on working code but not tested.
    git-svn-id: https://avr-cpp-lib.svn.sourceforge.net/svnroot/avr-cpp-lib/trunk@51 91aef0f8-0833-0410-9fe2-cf865ed08620

---
+++ AVRCppLib/SPI.h
@@ -0,0 +1,230 @@
+/**********************************************************************************************************************\
+
+	C++ library for Atmel AVR microcontrollers
+	Copyright (C) 2007 Lauri Kirikal, Mikk Leini, MTÜ TTÜ Robotiklubi
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+	See http://creativecommons.org/licenses/GPL/2.0/
+
+	MTÜ TTÜ Robotiklubi  http://www.robotiklubi.ee robotiklubi@gmail.com
+	Lauri Kirikal        laurikirikal@gmail.com
+	Mikk Leini           mikk.leini@gmail.com
+
+\**********************************************************************************************************************/
+
+#ifndef __AVR_CPP_SPI_H__
+#define __AVR_CPP_SPI_H__
+
+#include "IO.h"
+#include "Interrupt.h"
+
+
+/**********************************************************************************************************************\
+
+	AVRCppLib Serial Peripheral Interface
+	
+	Most AVR microcontroller have integrated SPI.
+	Most controllers with SPI have same functionality, register and bit names.
+	Therefore whole SPI code is in this one file.
+
+\**********************************************************************************************************************/
+
+namespace AVRCpp
+{
+	namespace SPI
+	{
+		enum OperationMode
+		{
+			SlaveMode   = 0x00,
+			MasterMode  = MSTR
+
+		}; // Operation mode
+
+		enum DataOrder
+		{
+			MSBFirst	= 0x00,
+			LSBFirst	= DORD
+
+		}; // DataOrder
+
+		enum ClockPolarity
+		{
+			ClockOnHigh = 0x00,
+			ClockOnLow  = CPOL
+
+		}; // ClockPolarity
+
+		enum ClockPhase
+		{
+			SampleOnLeadingEdge	= 0x00,
+			SampleOnFallingEdge = CPHA
+
+		}; // ClockPhase
+
+		enum ClockRate
+		{
+			RateDiv2	= 0x80,
+			RateDiv4	= 0x00,
+			RateDiv8	= 0x81,
+			RateDiv16	= 0x01,
+			RateDiv32	= 0x82,
+			RateDiv64	= 0x02,
+			RateDiv128	= 0x03,
+
+		}; // ClockRate
+		
+		namespace Internal
+		{
+			enum BitFlags
+			{
+				TransferEnableFlag		= 0x40,
+				DataOrderFlag			= 0x20,
+				MasterSlaveSelectFlag	= 0x10,
+				ClockPolarityFlag		= 0x08,
+				ClockPhaseFlag			= 0x04,
+				ClockRateFlag			= 0x03,
+
+				TransferCompleteFlag	= 0x80,
+				CollisionFlag			= 0x40,
+				DoubleSpeedFlag			= 0x01
+				
+			}; // enum BitFlags
+
+			/**
+			 *  SPI base structure
+			 */
+			template <
+						class ControlRegister,
+						class StatusRegister,
+						class DataRegister,
+						class SlaveSelectPin >
+
+			struct SPIbase
+			{
+				private:
+					typedef Bits<ControlRegister, TransferEnableFlag>		TransferEnableBit;
+					typedef Bits<ControlRegister, DataOrderFlag>			DataOrderBit;
+					typedef Bits<ControlRegister, MasterSlaveSelectFlag>	MasterSlaveSelectBit;
+					typedef Bits<ControlRegister, ClockPolarityFlag>		ClockPolarityBit;
+					typedef Bits<ControlRegister, ClockPhaseFlag>			ClockPhaseBit;
+					typedef Bits<ControlRegister, ClockRateFlag>			ClockRateBits;
+
+					typedef Bits<StatusRegister, TransferCompleteFlag>		TransferCompleteBit;
+					typedef Bits<StatusRegister, CollisionFlag>				CollisionBit;
+					typedef Bits<StatusRegister, DoubleSpeedFlag>			DoubleSpeedBit;
+
+				protected:
+					typedef DataRegister	Data;
+
+					/*  Why not this way ?
+					static inline void EnableTransferCompleteInterrupt() { SetBits<_SPCR>(_SPIE); }
+					static inline void DisableTransferCompleteInterrupt() { ClearBits<_SPCR>(_SPIE); }
+					static inline bool IsTransferCompleteInterruptEnabled() { return IsBitsSet<_SPCR>(_SPIE); }
+					*/
+
+					static inline bool volatile IsTransferCompleted() { return TransferCompleteBit::IsSet(); }
+					static inline bool WasWriteCollision() { return CollisionBit::IsSet(); }
+
+					static inline void WaitUntilTransferCompleted() { while (!IsTransferCompleted() && !WasWriteCollision()); }
+
+				public:
+
+					/**
+					 *  Setup SPI
+					 */
+					static inline void Setup (
+						OperationMode mode,
+						ClockRate rate,
+						DataOrder order,
+						ClockPolarity polarity,
+						ClockPhase phase )
+					{
+						ControlRegister::Set(SPE | order | mode | polarity | phase | (rate & ClockRateFlag));
+
+						if (rate & 0x80)
+							DoubleSpeedBit::Set();
+						else
+							DoubleSpeedBit::Clear();
+					}
+
+					/**
+					 *  Starts transmit by setting SPI to master mode and selecting slave device
+					 *	Only possible in master-mode
+					 */
+					static inline void StartTransmission()
+					{
+						//MasterSlaveSelectBit::Set();
+						SlaveSelectPin::Clear();
+					}
+
+					/**
+					 *  Ends transmitting by de-selecting slave device
+					 *  Only possible in master-mode
+					 */
+					static inline void EndTransmission()
+					{
+						SlaveSelectPin::Set();
+					}
+
+					/**
+					 *	Writes byte to SPI
+					 *
+					 *	@param uint8_t data
+					 *	@return bool
+					 */
+					static inline bool Write(uint8_t data)
+					{
+						WaitUntilTransferCompleted();
+
+						Data::Set(data);
+						
+						return !WasWriteCollision();
+					}
+
+					/**
+					 *	Reads byte from SPI
+					 *
+					 *	@param uint8_t &data
+					 *	@return bool
+					 */
+					static inline bool Read(uint8_t &data)
+					{
+						WaitUntilTransferCompleted();
+
+						return Data::Get();
+					}
+
+	   		}; // struct SPIbase
+   			
+		}; // namespace Internal
+
+	} // namespace SPI
+		
+}; // namespace AVRCpp
+
+
+/**********************************************************************************************************************/
+
+/* Devices supported and not */
+#if defined(__AVR_ATmega8__)
+#include "atmega8/SPI.h"
+#elif defined(__AVR_ATmega128__)
+#include "atmega128/SPI.h"
+#else
+#error "Device is not selected or selected device is not supported."
+#endif
+
+#endif // ifndef __AVR_CPP_SPI_H__
---
+++ AVRCppLib/atmega8/SPI.h
@@ -0,0 +1,72 @@
+/**********************************************************************************************************************\
+
+	C++ library for Atmel AVR microcontrollers
+	Copyright (C) 2007 Lauri Kirikal, Mikk Leini, MTÜ TTÜ Robotiklubi
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+	See http://creativecommons.org/licenses/GPL/2.0/
+
+	MTÜ TTÜ Robotiklubi  http://www.robotiklubi.ee robotiklubi@gmail.com
+	Lauri Kirikal        laurikirikal@gmail.com
+	Mikk Leini           mikk.leini@gmail.com
+
+\**********************************************************************************************************************/
+
+#ifndef __AVR_CPP_ATMEGA128_SPI_H__
+#define __AVR_CPP_ATMEGA128_SPI_H__
+
+/* This file should only be included from <avr/cpp/SPI.h>, never directly. */
+
+#ifndef __AVR_CPP_SPI_H__
+#ifdef __DOXYGEN__
+#include <avr/cpp/SPI.h>
+#else
+#error "Include <avr/cpp/SPI.h> instead of <avr/cpp/atmega8/SPI.h>."
+#endif
+#endif // ifndef __AVR_CPP_SPI_H__
+
+#ifndef __AVR_CPP_XXX_SPI_H__
+#define __AVR_CPP_XXX_SPI_H__ "atmega8/SPI.h"
+#else
+#error "Attempt to include more than one <avr/cpp/XXX/SPI.h> file."
+#endif
+
+namespace AVRCpp
+{
+	namespace SPI
+	{
+		// Functions from this namespace should not be called by user
+		namespace Internal
+		{
+			bool &Cancel();
+
+		}; // namespace Internal
+
+		struct SPI0 : Internal::SPIbase <
+				_SPCR,							/* ControlRegister */
+				_SPSR,							/* StatusRegister */
+				_SPDR,							/* DataRegister */
+				Pin2<PortB> >					/* SlaveSelectPin */
+		{
+			struct TransmissionCompleteInterrupt : BasicInterrupt<Bits<_SPCR, _SPIE> > { __INTERRUPT_HANDLER_SUPPORT__ };
+
+		}; // struct SPI0
+
+	} // namespace SPI
+
+} // namespace AVRCpp
+
+#endif // ifndef __AVR_CPP_ATMEGA8_SPI_H__
