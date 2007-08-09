07a0debe4c131b91d0de1f0af5a8f65c33f39d1e
lauri_kirikal <lauri_kirikal@91aef0f8-0833-0410-9fe2-cf865ed08620>
2007-08-09 17:57:48 +0000

    All in one file (Sleeping.h). Folder 'common' is now deprecated.
    git-svn-id: https://avr-cpp-lib.svn.sourceforge.net/svnroot/avr-cpp-lib/trunk@60 91aef0f8-0833-0410-9fe2-cf865ed08620

--- AVRCppLib/Sleeping.h
+++ AVRCppLib/Sleeping.h
@@ -24,20 +24,43 @@
 	Mikk Leini           mikk.leini@gmail.com
 
 \**********************************************************************************************************************/
-
-#if (defined(__AVR_ATmega8__) || defined(__AVR_ATmega128__) || defined(__AVR_ATmega64__) || defined(__AVR_ATmega88__) || defined(__AVR_AT90USB1287__))
-#define __SLEEPING_TYPE1__
-#elif defined(__AVR_ATmega8515__)
-#define __SLEEPING_TYPE2__
-#endif
-
-#if !(defined(__SLEEPING_TYPE1__) || defined(__SLEEPING_TYPE2__))
-#error "Device is not selected or selected device is not supported."
-#define __AVR_CPP_SLEEPING_H__
-#endif
-
 #ifndef __AVR_CPP_SLEEPING_H__
 #define __AVR_CPP_SLEEPING_H__
+
+
+#if defined(__AVR_ATmega48__) || defined(__AVR_ATmega88__) || defined(__AVR_ATmega168__)
+
+#define __SLEEPING_NUMBER_OF_MODES__	5
+#define __SLEEPING_REGISTER__			_SMCR
+
+#if defined(__AVR_AT90USB1287__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega324__) || defined(_AVR_ATmega164__)
+
+#define __SLEEPING_NUMBER_OF_MODES__	6
+#define __SLEEPING_REGISTER__			_SMCR
+
+#elif defined(__AVR_ATmega8__)
+
+#define __SLEEPING_NUMBER_OF_MODES__	5
+#define __SLEEPING_REGISTER__			_MCUCR
+
+#elif defined(__AVR_ATmega128__) || defined(__AVR_ATmega64__)
+
+#define __SLEEPING_NUMBER_OF_MODES__	6
+#define __SLEEPING_REGISTER__			_MCUCR
+
+#elif defined(__AVR_ATmega8515__)
+
+#define __SLEEPING_NUMBER_OF_MODES__	3
+// __SLEEPING_REGISTER__ is not defined here because bits are scattered into various registers.
+
+#else
+
+#error "Device is not selected or selected device is not supported."
+
+#endif
+
+
+#if defined(__SLEEPING_NUMBER_OF_MODES__)
 
 #include "IO.h"
 #include "Assembler.h"
@@ -53,17 +76,8 @@ namespace AVRCpp
 			
 		}; // EnabledFlag
 		
-#ifdef __AVR_ATmega8515__
 		
-		enum SleepMode
-		{
-			Idle				= 0x00,	
-			PowerDown			= _SM1,
-			Standby				= _SM2 | _SM1,
-			
-		}; // enum SleepMode
-		
-#elif (defined(__AVR_ATmega128__) || defined(__AVR_ATmega64__) || defined(__AVR_ATmega88__) || defined(__AVR_AT90USB1287__) )
+#if __SLEEPING_NUMBER_OF_MODES__ == 6
 		
 		enum SleepMode
 		{
@@ -76,7 +90,7 @@ namespace AVRCpp
 			
 		}; // enum SleepMode
 		
-#elif __AVR_ATmega8__
+#elif __SLEEPING_NUMBER_OF_MODES__ == 5
 		
 		enum SleepMode
 		{
@@ -84,23 +98,82 @@ namespace AVRCpp
 			ADCNoiseReduction	= _SM0,
 			PowerDown			= _SM1,
 			PowerSave			= _SM1 | _SM0,
-			Standby				= _SM2 | _SM1,
+			Standby				= _SM2 | _SM1
 			
 		}; // enum SleepMode
 		
-#endif
+#if __SLEEPING_NUMBER_OF_MODES__ == 3
+		
+		enum SleepMode
+		{
+			Idle				= 0x00,	
+			PowerDown			= _SM1,
+			Standby				= _SM2 | _SM1
+			
+		}; // enum SleepMode
+		
+#endif // if __SLEEPING_NUMBER_OF_MODES__ == ?
+		
+		
+#ifdef __SLEEPING_REGISTER__
+
+		inline void SetUp(SleepMode sleepMode, EnabledFlag enabled)
+		{
+			ChangeBits<__SLEEPING_REGISTER__>(_SM0 | _SM1 | _SM2 | _SE, sleepMode | enabled);
+			
+		} // SetUp
+		
+		inline void FallAsleep() { Assembler::SLEEP(); }
+		
+		inline void Enable() { SetBits<__SLEEPING_REGISTER__>(_SE); }
+		inline void Disable() { ClearBits<__SLEEPING_REGISTER__>(_SE); }
+		inline uint8_t IsEnabled() { return IsBitsSet<__SLEEPING_REGISTER__>(_SE); }
+		
+		inline uint8_t GetSleepMode() { return SelectBits<__SLEEPING_REGISTER__>(_SM0 | _SM1 | _SM2); }
+		inline void SetSleepMode(SleepMode sleepMode) { ChangeBits<__SLEEPING_REGISTER__>(_SM0 | _SM1 | _SM2, sleepMode); }
+		
+#undef __SLEEPING_REGISTER__
+
+#else // __SLEEPING_REGISTER__ not defined
+
+		inline void SetUp(SleepMode sleepMode, EnabledFlag enabled)
+		{
+			ChangeBits<_MCUCR>(_SM1 | _SE, sleepMode | enabled);
+			ChangeBits<_MCUCSR>(_SM2, sleepMode);
+			ChangeBits<_EMCUCR>(_SM0, sleepMode);
+			
+		} // SetUp
+		
+		inline void FallAsleep() { Assembler::SLEEP(); }
+		
+		inline void Enable() { SetBits<_MCUCR>(_SE); }
+		inline void Disable() { ClearBits<_MCUCR>(_SE); }
+		inline uint8_t IsEnabled() { return IsBitsSet<_MCUCR>(_SE); }
+		
+		inline uint8_t GetSleepMode()
+		{
+			return ( SelectBits<_MCUCR>(_SM1)
+					| SelectBits<_MCUCSR>(_SM2)
+					| SelectBits<_EMCUCR>(_SM0) );
+
+		} // GetSleepMode
+
+		inline void SetSleepMode(SleepMode sleepMode)
+		{
+			ChangeBits<_MCUCR>(_SM1, sleepMode);
+			ChangeBits<_MCUCSR>(_SM2, sleepMode);
+			ChangeBits<_EMCUCR>(_SM0, sleepMode);
+
+		} // SetSleepMode
+
+#endif // ifdef __SLEEPING_REGISTER__
+
 		
 	} // namespace Sleeping
 
 } // namespace AVRCpp
 
 
-#if defined(__SLEEPING_TYPE1__)
-#include "common/Sleeping1.h"
-#elif defined(__SLEEPING_TYPE2__)
-#include "common/Sleeping2.h"
-#else
-#error "Device is not selected or selected device is not supported."
-#endif
+#endif // if defined(__SLEEPING_NUMBER_OF_MODES__)
 
 #endif // ifndef __AVR_CPP_SLEEPING_H__
