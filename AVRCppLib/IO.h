1c58cfd9481a2a64976876703b2b645a302b5435
lauri_kirikal <lauri_kirikal@91aef0f8-0833-0410-9fe2-cf865ed08620>
2008-02-19 17:40:53 +0000

    New release of WinAVR is out.
    git-svn-id: https://avr-cpp-lib.svn.sourceforge.net/svnroot/avr-cpp-lib/trunk@128 91aef0f8-0833-0410-9fe2-cf865ed08620

--- AVRCppLib/IO.h
+++ AVRCppLib/IO.h
@@ -43,8 +43,8 @@
 #else
 
 // Compatibility test
-#if !(defined(__AVR__) && __GNUC__ == 4 && __GNUC_MINOR__ == 1 && __GNUC_PATCHLEVEL__ == 2)
-#warning "AVR C++ Lib is tested on avr-gcc (GCC) 4.1.2 (WinAVR 20070525). You are not using this compiler. AVR C++ Lib may not work properly."
+#if !(defined(__AVR__) && __GNUC__ == 4 && __GNUC_MINOR__ == 2 && __GNUC_PATCHLEVEL__ == 2)
+#warning "AVR C++ Lib is tested on avr-gcc (GCC) 4.2.2 (WinAVR 20071221). You are not using this compiler. AVR C++ Lib may not work properly."
 #endif
 
 namespace AVRCpp
