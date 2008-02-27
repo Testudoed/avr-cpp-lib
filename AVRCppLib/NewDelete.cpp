91d8007b53ea7bd185dbb4c267cbdf1b3698ee3e
lauri_kirikal <lauri_kirikal@91aef0f8-0833-0410-9fe2-cf865ed08620>
2008-02-27 11:35:16 +0000

    No license
    git-svn-id: https://avr-cpp-lib.svn.sourceforge.net/svnroot/avr-cpp-lib/trunk@133 91aef0f8-0833-0410-9fe2-cf865ed08620

--- AVRCppLib/NewDelete.cpp
+++ AVRCppLib/NewDelete.cpp
@@ -1,30 +1,3 @@
-/**********************************************************************************************************************\
-
-	C++ library for Atmel AVR microcontrollers
-	Copyright (C) 2007 Lauri Kirikal, Mikk Leini, Rasmus Raag, MTÜ TTÜ Robotiklubi
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-	See http://creativecommons.org/licenses/GPL/2.0/
-
-	MTÜ TTÜ Robotiklubi  http://www.robotiklubi.ee robotiklubi@gmail.com
-	Lauri Kirikal        laurikirikal@gmail.com
-	Mikk Leini           mikk.leini@gmail.com
-	Rasmus Raag          rasmus_raag@users.sourceforge.net
-
-\**********************************************************************************************************************/
 
 extern "C++" {
 
@@ -34,7 +7,7 @@ extern "C++" {
 		typedef void (*new_handler)();
 		new_handler set_new_handler(new_handler) throw();
 	}
-	
+
 	void operator delete(void*) throw();
 	void operator delete[](void*) throw();
 	void* operator new(std::size_t) throw();
